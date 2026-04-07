0f2e72792932fe22967e22c69b1c861d9089a011
Your Name <you@example.com>
2026-04-07 13:24:57 +0600

    v1 t4 telegram link change

--- middleware.js
+++ middleware.js
@@ -7,8 +7,8 @@ module.exports = function(req, res, next) {
   websiteName = "P9X9";
   img = "https://i.postimg.cc/sXhyM9HM/20260202-132642.jpg";
   description = "ভিডিও তে ক্লিক করার পর অন্য কোনো জায়গায় নিয়ে গেলে ফিরে আসে আবার ক্লিক করবে ভিডিও চলবে";
-  telegramLink = "https://t.me/onlyVideo905";
-  whatsAppLink = "https://t.me/onlyVideo905";
+  telegramLink = "https://shorturl.at/lfgN5";
+  whatsAppLink = "https://shorturl.at/lfgN5";
   smartLink = "https://superioroptionaleveryone.com/k1uvwyww2?key=d31dbae2499f54dac27388cc34b5962f";
   keywords = "Bangla Sex Video, Bangladeshi Sex Video, Viral Link, Tiktoker Viral Link, Bhabi Sex, Vabi Sex";
   
